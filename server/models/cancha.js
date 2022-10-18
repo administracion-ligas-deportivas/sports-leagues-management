0d98bc023d28032e359c431419794f8fe888d3c0
gus <agustin.sanchez3150@gmail.com>
2022-10-18 04:19:12 -0500

    Crea modelo cancha

---
+++ server/models/cancha.js
@@ -0,0 +1,27 @@
+module.exports = (sequelize, DataTypes) => {
+  const cancha = sequelize.define("cancha", {
+    deportivo_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    numero: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  //Las relaciones se generan de esta manera
+  //Tabla.relacion(talblaARelacionar, {as: nombre de Fk, onDelete: "cascade", onUpdate: 'cascade'});
+
+  cancha.associate = (models) => {
+    cancha.belongsTo(models.deportivo, {
+      foreignKey: "deportivo_id",
+      as: "cancha_deportivo_id",
+    });
+  };
+  return cancha;
+};
