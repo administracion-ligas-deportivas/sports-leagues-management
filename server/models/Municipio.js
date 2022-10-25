31b92e878dfb7b9fd7adf0747d4e73302f26847b
gus <agustin.sanchez3150@gmail.com>
2022-10-25 14:35:13 -0500

    Crea relacion Municipio

--- server/models/Municipio.js
+++ server/models/Municipio.js
@@ -1,24 +1,24 @@
-module.exports = (sequelize, DataTypes) => 
-{
-    const Municipio = sequelize.define('Municipio', {
-        nombre: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        estado_id:
-        {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
-    });
+module.exports = (sequelize, DataTypes) => {
+  const Municipio = sequelize.define("Municipio", {
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    estado_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
 
-    Municipio.assosiate = function(models){
-        Municipio.belongsTo(models.Estado, {foreignKey: 'estado_id', as: 'municipio_estado_id'});
-    };
+  Municipio.assosiate = function (models) {
+    Municipio.hasMany(models.domicilio_usuario, { as: "domicilio_usuario" });
+  };
+  Municipio.assosiate = function (models) {
+    Municipio.belongsTo(models.Estado);
+  };
 
-    Municipio.assosiate = function(models) {
-        Municipio.hasMany(models.Usuario, {as: 'usuarios'});
-    };
-
-    return Municipio;
+  Municipio.assosiate = function (models) {
+    Municipio.hasMany(models.Usuario, { as: "usuario" });
+  };
+  return Municipio;
 };
