e426f995659e3fb0d5cc7cbc77c43298c17ac6da
gus <agustin.sanchez3150@gmail.com>
2022-10-25 21:53:30 -0500

    Crea relacion partido

--- server/models/partido.js
+++ server/models/partido.js
@@ -26,13 +26,18 @@ module.exports = (sequelize, DataTypes) => {
   //Tabla.relacion(talblaARelacionar, {as: nombre de Fk, onDelete: "cascade", onUpdate: 'cascade'});
 
   partido.associate = (models) => {
-    partido.belongsTo(models.evento_deportivo, {
-      foreignKey: "evento_deportivo_id",
-      as: "partido_evento_deportivo_id",
+    partido.hasMany(models.estadistica_jugador_partido, {
+      foreignKey: "partido_id",
+    });
+    partido.hasMany(models.equipo_partido, {
+      foreignKey: "partido_id",
+      as: "equipo_partido_partido_id",
     });
     partido.belongsTo(models.cancha, {
       foreignKey: "cancha_id",
-      as: "partido_cancha_id",
+    });
+    partido.belongsTo(models.Usuario, {
+      foreignKey: "estadistico_id",
     });
   };
   return partido;
