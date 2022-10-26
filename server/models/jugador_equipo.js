0e2f05ecdabde086a55c04fe19d12ccb1cdec617
gus <agustin.sanchez3150@gmail.com>
2022-10-26 12:36:57 -0500

    Crea relacion jugador_equipo

--- server/models/jugador_equipo.js
+++ server/models/jugador_equipo.js
@@ -13,7 +13,6 @@ module.exports = (sequelize, DataTypes) => {
   jugador_equipo.associate = (models) => {
     jugador_equipo.belongsTo(models.Usuario, {
       foreignKey: "usuario_id",
-      as: "jugador_equipo_usuario_id",
     });
     jugador_equipo.belongsTo(models.equipo, {
       foreignKey: "equipo_id",
