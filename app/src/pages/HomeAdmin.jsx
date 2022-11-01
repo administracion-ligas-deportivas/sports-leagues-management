8994e6ba2d2173921c59fe486e3635e1590d8f9a
yeicobF <jacobrdz34@gmail.com>
2022-11-01 17:57:27 -0600

    Direccion rutas botones a nuevas
    Faltan algunas de definir en el archivo de rutas, pero las pantallas sí
    están.

--- app/src/pages/HomeAdmin.jsx
+++ app/src/pages/HomeAdmin.jsx
@@ -2,67 +2,46 @@ import { Link } from "react-router-dom";
 import { Button, Stack } from "@mui/material";
 import style from "@/styles/Home.module.css";
 
+const pages = [
+  { to: "/create-advise", text: "Crear Aviso" },
+  { to: "/gestion-torneo", text: "Gestionar Torneo" },
+  { to: "/nuevo-arbitro", text: "Nuevo Arbitro " },
+  { to: "/crear-evento-deportivo", text: "Crear Evento Deportivo" },
+  { to: "/enter-sports-event", text: "Entrar a Evento Deportivo" },
+  { to: "/estadisticas-personales", text: "Estadisticas Personales" },
+  { to: "/gestion-equipo-jugador", text: "Gestión de equipo jugador" },
+  { to: "/asignar-roles", text: "Asignar Roles" },
+  { to: "/registro-deportivo", text: "Registro deportivo" },
+  { to: "/traspaso-equipo", text: "Traspaso de equipo" },
+  { to: "/nueva-cancha", text: "Nueva Cancha" },
+  { to: "/eventos-deportivos", text: "Eventos Deportivos" },
+  { to: "/registro-estadistico", text: "Registrar Estadistico" },
+  { to: "/equipos-en-sistema", text: "Equipos en sistema" },
+  { to: "/gestion-equipo", text: "Gestión de equipo" },
+  { to: "/registro-pago-fisico", text: "Registro de pago físico " },
+];
+
 function HomeAdmin() {
   return (
     <>
       <div className={style.container}>
         <h2>Pantallas Realizadas</h2>
-        <Stack direction="row" spacing={2}>
-          <Link to="/CreateAdvise" className={style.link}>
-            <Button variant="contained"> Crear Aviso </Button>
-          </Link>
-          <Link to="/gestion-torneo" className={style.link}>
-            <Button variant="contained"> Gestionar Torneo </Button>
-          </Link>
-          <Link to="/NuevoArbitro" className={style.link}>
-            <Button variant="contained"> Nuevo Arbitro </Button>
-          </Link>
-          <Link to="/CrearEventoDeportivo" className={style.link}>
-            <Button variant="contained"> Crear Evento Deportivo </Button>
-          </Link>
-          <Link to="/EnterSportsEvent" className={style.link}>
-            <Button variant="contained"> Entrar a Evento Deportivo</Button>
-          </Link>
-          <Link to="/EstadisticasPersonales" className={style.link}>
-            <Button variant="contained"> Estadisticas Personales</Button>
-          </Link>
-        </Stack>
-        <br></br>
-        <Stack direction="row" spacing={2}>
-          <Link to="/GestionEquipoJugador" className={style.link}>
-            <Button variant="contained"> Gestión de equipo jugador</Button>
-          </Link>
-          <Link to="/AsignarRoles" className={style.link}>
-            <Button variant="contained"> Asignar Roles </Button>
-          </Link>
-          <Link to="/RegistroDeportivo" className={style.link}>
-            <Button variant="contained"> Registro deportivo </Button>
-          </Link>
-          <Link to="/TraspasoEquipo" className={style.link}>
-            <Button variant="contained"> Traspaso de equipo </Button>
-          </Link>
-          <Link to="/NuevaCancha" className={style.link}>
-            <Button variant="contained"> Nueva Cancha </Button>
-          </Link>
-          <Link to="/EventosDeportivos" className={style.link}>
-            <Button variant="contained"> Eventos Deportivos</Button>
-          </Link>
-        </Stack>
-        <br></br>
-        <Stack direction="row" spacing={2}>
-          <Link to="/RegistroEstadistico" className={style.link}>
-            <Button variant="contained"> Registrar Estadistico</Button>
-          </Link>
-          <Link to="/EquiposEnSistema" className={style.link}>
-            <Button variant="contained"> Equipos en sistema</Button>
-          </Link>
-          <Link to="/GestionEquipo" className={style.link}>
-            <Button variant="contained"> Gestión de equipo</Button>
-          </Link>
-          <Link to="/RegistroPagoFisico" className={style.link}>
-            <Button variant="contained"> Registro de pago físico </Button>
-          </Link>
-        </Stack>
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: ".5rem",
+          }}
+        >
+          {pages.map((page) => {
+            return (
+              <Link key={page.to} to={page.to} className={style.link}>
+                <Button variant="contained">{page.text}</Button>
+              </Link>
+            );
+          })}
+        </div>
+        <div>{pages.length} pantallas</div>
       </div>
     </>
   );
