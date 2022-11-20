8aafdbdf68b0d75b27080d7ce5d390e5a834499c
gus <agustin.sanchez3150@gmail.com>
2022-11-20 00:55:54 -0600

    foreignKey repetitivas eliminadas PermisoRol

--- api/src/models/PermisoRol.js
+++ api/src/models/PermisoRol.js
@@ -1,28 +1,11 @@
-module.exports = (sequelize, DataTypes) => {
-  const PermisoRol = sequelize.define(
-    "PermisoRol",
-    {
-      rol_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      permiso_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-    },
-    {
-      paranoid: true,
-    }
-  );
+module.exports = (sequelize /* DataTypes */) => {
+  const PermisoRol = sequelize.define("PermisoRol", {
+    paranoid: true,
+  });
 
   PermisoRol.associate = (models) => {
-    PermisoRol.belongsTo(models.rol, {
-      foreignKey: "rol_id",
-    });
-    PermisoRol.belongsTo(models.permiso, {
-      foreignKey: "permiso_id",
-    });
+    PermisoRol.belongsTo(models.rol);
+    PermisoRol.belongsTo(models.permiso);
   };
   return PermisoRol;
 };
