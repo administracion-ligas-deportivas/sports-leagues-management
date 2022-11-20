fc961bccf64cf5f303a1a1473c7b9bb10961d7fa
gus <agustin.sanchez3150@gmail.com>
2022-11-20 00:45:16 -0600

    ForeignKeys repetitivas eliminadas PagoEventoDeportivo

--- api/src/models/PagoEventoDeportivo.js
+++ api/src/models/PagoEventoDeportivo.js
@@ -2,18 +2,6 @@ module.exports = (sequelize, DataTypes) => {
   const PagoEventoDeportivo = sequelize.define(
     "PagoEventoDeportivo",
     {
-      usuario_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      evento_deportivo_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      equipo_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
       monto: {
         type: DataTypes.FLOAT,
         allowNull: false,
@@ -41,16 +29,10 @@ module.exports = (sequelize, DataTypes) => {
   );
 
   PagoEventoDeportivo.associate = (models) => {
-    PagoEventoDeportivo.belongsTo(models.Usuario, {
-      foreignKey: "usuario_id",
-    });
+    PagoEventoDeportivo.belongsTo(models.Usuario);
 
-    PagoEventoDeportivo.belongsTo(models.EventoDeportivo, {
-      foreignKey: "evento_deportivo_id",
-    });
-    PagoEventoDeportivo.belongsTo(models.Equipo, {
-      foreignKey: "equipo_id",
-    });
+    PagoEventoDeportivo.belongsTo(models.EventoDeportivo);
+    PagoEventoDeportivo.belongsTo(models.Equipo);
   };
 
   return PagoEventoDeportivo;
