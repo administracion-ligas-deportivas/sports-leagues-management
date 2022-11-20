878a79c9fc817cdfb69159546d9c3903fe849ad5
gus <agustin.sanchez3150@gmail.com>
2022-11-19 21:56:57 -0600

    Elimina foregeinKey repetitivas estadistica_deporte
    Elimina relaciones establecidas dentro de associate y atributos foreign key

--- api/src/models/estadistica_deporte.js
+++ api/src/models/estadistica_deporte.js
@@ -1,28 +1,11 @@
-module.exports = (sequelize, DataTypes) => {
-  const EstadisticaDeporte = sequelize.define(
-    "EstadisticaDeporte",
-    {
-      estadistica_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      deporte_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-    },
-    {
-      paranoid: true,
-    }
-  );
+module.exports = (sequelize /* DataTypes */) => {
+  const EstadisticaDeporte = sequelize.define("EstadisticaDeporte", {
+    paranoid: true,
+  });
 
   EstadisticaDeporte.associate = (models) => {
-    EstadisticaDeporte.belongsTo(models.Deporte, {
-      foreignKey: "deporte_id",
-    });
-    EstadisticaDeporte.belongsTo(models.estadistica, {
-      foreignKey: "estadistica_id",
-    });
+    EstadisticaDeporte.belongsTo(models.Deporte);
+    EstadisticaDeporte.belongsTo(models.estadistica);
   };
   return EstadisticaDeporte;
 };
