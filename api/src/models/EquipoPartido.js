16134bbdda165cad499415147e10db33b71496e5
gus <agustin.sanchez3150@gmail.com>
2022-11-19 21:42:51 -0600

    Elimina referencia a foreign key y atributo foreign Key en EquipoPartido

--- api/src/models/EquipoPartido.js
+++ api/src/models/EquipoPartido.js
@@ -2,14 +2,6 @@ module.exports = (sequelize, DataTypes) => {
   const EquipoPartido = sequelize.define(
     "EquipoPartido",
     {
-      partido_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      equipo_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
       puntos: {
         type: DataTypes.INTEGER,
         allowNull: true,
@@ -25,12 +17,8 @@ module.exports = (sequelize, DataTypes) => {
   );
 
   EquipoPartido.associate = (models) => {
-    EquipoPartido.belongsTo(models.partido, {
-      foreignKey: "partido_id",
-    });
-    EquipoPartido.belongsTo(models.equipo, {
-      foreignKey: "equipo_id",
-    });
+    EquipoPartido.belongsTo(models.partido);
+    EquipoPartido.belongsTo(models.equipo);
   };
   return EquipoPartido;
 };
