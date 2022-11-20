e54b1333d34149c5a2c94a64d8a80a71b059add1
gus <agustin.sanchez3150@gmail.com>
2022-11-20 00:25:16 -0600

    atributos FormatoEventoDeportivo a camelCase

--- api/src/models/FormatoEventoDeportivo.js
+++ api/src/models/FormatoEventoDeportivo.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.TEXT,
         allowNull: false,
       },
-      maximo_equipos: {
+      maximoEquipos: {
         type: DataTypes.INTEGER,
         allowNull: false,
       },
