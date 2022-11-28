cc9cb56e3f04a3520e3e4f18e3bc0c38710976bb
yeicobF <jacobrdz34@gmail.com>
2022-11-27 21:19:09 -0600

    Style(api): Limpiar archivo

--- api/src/db/seeders/20221121100412-usuarios.js
+++ api/src/db/seeders/20221121100412-usuarios.js
@@ -1,10 +1,10 @@
 "use strict";
 
+const { usuario: usuarioModel, domicilioUsuario } = require("../models");
 const {
   createRandomUsers,
   createRandomAddress,
 } = require("../../utils/fakeDataGenerators/usuarios");
-const { usuario: usuarioModel, domicilioUsuario } = require("../models");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
@@ -41,89 +41,6 @@ module.exports = {
         })
       );
     });
-    // const usuarioModel = usuario(
-    //   queryInterface.sequelize,
-    //   Sequelize.DataTypes
-    // );
-    // const domicilioUsuarioModel = domicilioUsuario(
-    //   queryInterface.sequelize,
-    //   Sequelize.DataTypes
-    // );
-
-    //         await Promise.all(
-    //           usuarios.forEach(async (usuario) => {
-    //             const { id } = usuario;
-    //             const hasDomicilioUsuario = Boolean(
-    //               await usuario.getDomicilioUsuario()
-    //             );
-    //             console.log({ hasDomicilioUsuario, id });
-    //
-    //             //             if (hasDomicilioUsuario) return;
-    //             //
-    //             //             console.log({ id });
-    //             //             const domicilioUsuario = createRandomAddress(id);
-    //             //             await usuario.setDomicilioUsuario(domicilioUsuario);
-    //           })
-    //         );
-    // .then((usuarios) =>
-    //   usuarios.map((usuario) => {
-    //     return usuario.getDomicilioUsuario();
-    //   })
-    // );
-
-    //         const transformedUsuarios = await Promise.all(
-    //           usuarios.map(async (usuario) => {
-    //             const hasDomicilioUsuario = Boolean(
-    //               await usuario.getDomicilioUsuario()
-    //             );
-    //
-    //             return {
-    //               ...usuario.toJSON(),
-    //               hasDomicilioUsuario,
-    //             };
-    //           })
-    //         );
-    // console.log({ transformedUsuarios });
-
-    // // https://stackoverflow.com/a/48753999/13562806
-    // const usuariosDb = await queryInterface.sequelize.query(
-    //   "SELECT id from usuario",
-    //   {
-    //     type: Sequelize.QueryTypes.SELECT,
-    //     transaction,
-    //   }
-    // );
-
-    //         const randomAddresses = usuariosDb.map(({ id }) =>
-    //           createRandomAddress(id)
-    //         );
-    //
-    //         await queryInterface.bulkInsert("domicilio_usuario", randomAddresses, {
-    //           transaction,
-    //         });
-    //         const model = queryInterface.sequelize.model("usuario");
-    //
-    //         console.log({ model });
-    //
-    //         // Asignar a cada usuario un domicilio.
-    //         const usuariosDb = await usuarioModel.findAll({
-    //           include: [{ model: domicilioUsuario }],
-    //         });
-    //
-    //         console.log({ usuariosDb });
-    //
-    //         const promises = usuariosDb.map(async (usuario) => {
-    //           const domicilioUsuario = createRandomAddress();
-    //
-    //           if (!usuario.getDomicilioUsuario()) return;
-    //
-    //           return await usuario.createDomicilioUsuario(domicilioUsuario);
-    //         });
-    //
-    //         const usuariosConDomicilio = await Promise.all(promises);
-    //
-    //         console.log({ usuariosConDomicilio });
-    //         return usuariosConDomicilio;
   },
 
   async down(queryInterface, Sequelize) {
