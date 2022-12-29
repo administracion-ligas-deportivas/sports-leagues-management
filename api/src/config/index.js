13c9acea38ee8eb430b54a343733bf9697edfe68
yeicobF <jacobrdz34@gmail.com>
2022-12-28 19:45:53 -0600

    Refactor(api): Modificar implementación de inicialización

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -1,5 +1,15 @@
+const devConfig = require("./dev");
+const { faker } = require("@faker-js/faker");
+const middlewaresConfig = require("./middlewares");
+const routesConfig = require("./routes");
+
 const PORT = process.env.PORT || 3001;
+
+faker.setLocale("es_MX");
 
 module.exports = {
   PORT,
+  ...devConfig,
+  ...middlewaresConfig,
+  ...routesConfig,
 };
