af3233f8ca0650b099b58925809c9f458766df15
yeicobF <jacobrdz34@gmail.com>
2022-12-31 06:50:56 -0600

    Refactor(api): Simplificar createPartido
    Con un middleware que verifique si los equipos se enviaron correctamente
    y los mande en req, se facilitaría el proceso. O eso creo.

--- api/src/services/partidos/partidos.js
+++ api/src/services/partidos/partidos.js
@@ -2,8 +2,54 @@ const { LOCAL_VISITANTE } = require("#src/constants/partidos.js");
 const { partido, equipo } = require("#src/db/models/index.js");
 
 /**
+ * Crear un partido y añadir los equipos local y visitante.
+ *
+ * Revisar que los equipos estén inscritos en el evento deportivo. Podríamos
+ * utilizar el `rest` operator para iterar por cada equipo. También verificar
+ * que el estadístico seleccionado estén inscrito en el evento deportivo.
+ */
+const createPartido = async (eventoDeportivoId, partidoInfo) => {
+  const { equipos, estadisticoId, canchaId, ...restInfo } = partidoInfo;
+  const { local, visitante } = equipos;
+
+  const createdPartido = await partido.create({
+    ...restInfo,
+    eventoDeportivoId,
+    estadisticoId,
+    canchaId,
+  });
+
+  const addedEquipos = await Promise.all(
+    Object.entries(equipos).map(async ([localVisitanteKey, { id }]) => {
+      const foundEquipo = await equipo.findByPk(id);
+
+      const isLocal = localVisitanteKey === LOCAL_VISITANTE.LOCAL;
+      const puntos = isLocal ? local.puntos : visitante.puntos;
+      const key = localVisitanteKey.toUpperCase();
+
+      return await createdPartido.addEquipo(foundEquipo, {
+        through: {
+          puntos: puntos ?? 0,
+          // local: equipo's instance
+          // visitante: equipo's instance
+          localVisitante: LOCAL_VISITANTE[key],
+        },
+      });
+    })
+  );
+
+  // https://stackoverflow.com/questions/10865025/merge-flatten-an-array-of-arrays
+  return {
+    partido: createdPartido,
+    equipos: addedEquipos.flat(),
+  };
+};
+
+/**
  * Crear un partido y añadir los equipos, que son instancias del modelo
  * `equipo`. No se añaden mediante su ID.
+ *
+ * **Esta función la utilicé más que nada para generar los `seeders`.**
  *
  * @param {Object} equipos Objeto con las propiedades `local` y `visitante`.
  * Cada propiedad tiene `equipo` y `puntos` como propiedades. El equipo es una
@@ -63,115 +109,7 @@ const createPartidoAndAddEquipos = async (
   return createdPartido;
 };
 
-// Revisar que los equipos estén inscritos en el evento deportivo. Podríamos
-// utilizar el `rest` operator para iterar por cada equipo. También verificar
-// que el estadístico seleccionado estén inscrito en el evento deportivo.
-const createPartido = async (eventoDeportivoId, partidoInfo) => {
-  const { equipos, estadisticoId, canchaId, ...restInfo } = partidoInfo;
-  const { local, visitante } = equipos;
-
-  const foundEquipos = await Promise.all(
-    Object.entries(equipos).map(async ([localVisitanteKey, { id }]) => {
-      const foundEquipo = await equipo.findOne({
-        where: { id },
-      });
-
-      const isLocal = localVisitanteKey === LOCAL_VISITANTE.LOCAL;
-      const puntos = isLocal ? local.puntos : visitante.puntos;
-      const key = localVisitanteKey.toUpperCase();
-
-      return {
-        // [localVisitanteKey]: {
-        instance: foundEquipo,
-        through: {
-          puntos: puntos ?? 0,
-          // local: equipo's instance
-          // visitante: equipo's instance
-          localVisitante: LOCAL_VISITANTE[key],
-        },
-        // },
-      };
-    })
-  );
-
-  // console.log({ foundEquipos: JSON.stringify(foundEquipos) });
-
-  // https://stackoverflow.com/a/49247635/13562806
-  /* const { local: foundLocal, visitante: foundVisitante } = Object.assign(
-    {},
-    ...foundEquipos
-  );
-
-  console.log({
-    foundEquipos: Object.values(foundEquipos).map((equipo) =>
-      JSON.stringify(equipo.instance, null, 2)
-    ),
-  });
-  console.log({ foundLocal, foundVisitante }); */
-
-  //   const foundLocal = await equipo.findOne({
-  //     where: { id: local.id },
-  //   });
-  //
-  //   const foundVisitante = await equipo.findOne({
-  //     where: { id: visitante.id },
-  //   });
-
-  /* if (!foundLocal || !foundVisitante) {
-    throw new Error("No se encontró el equipo");
-  } */
-
-  // const equiposInfo = {
-  //   through: {
-  //     puntos: [local.puntos ?? 0, visitante.puntos ?? 0],
-  //     localVisitante: [LOCAL_VISITANTE.LOCAL, LOCAL_VISITANTE.VISITANTE],
-  //   },
-  // };
-
-  // const foundEquipos = [foundLocal, foundVisitante];
-
-  const partidoData = {
-    ...restInfo,
-    eventoDeportivoId,
-    estadisticoId,
-    canchaId,
-  };
-
-  console.log({ partidoData });
-
-  const createdPartido = await partido.create(partidoData);
-
-  // console.log({ createdPartido });
-
-  // const partidoWithEquipos = await createdPartido.addEquipos(
-  //   foundEquipos,
-  //   equiposInfo
-  // );
-
-  // return await createdPartido.addEquipo(equipo, {
-  //   through: { puntos, localVisitante: localVisitanteKey },
-  //   transaction,
-  // });
-
-  const addedEquipos = await Promise.all(
-    foundEquipos.map(async (equipo) => {
-      const { instance, through } = equipo;
-      console.log({ equipo });
-
-      return await createdPartido.addEquipo(instance, {
-        through,
-      });
-    })
-  );
-
-  // https://stackoverflow.com/questions/10865025/merge-flatten-an-array-of-arrays
-  return {
-    partido: createdPartido,
-    equipos: addedEquipos.flat(),
-  };
-};
-
 module.exports = {
-  createPartidoAndAddEquipos,
   createPartido,
+  createPartidoAndAddEquipos,
 };
