16385f32002efbe69a111ac61760fcb641e075d3
yeicobF <jacobrdz34@gmail.com>
2023-01-02 14:29:46 -0600

    Build(api): No mostrar SQL al crear iniciar server
    Al iniciar la aplicación se mostraban todas las queries para crear la BD
    y esto tomaba un buen tiempo, alentando el inicio del servidor. Ahora ya
    no se encuentra el "logging" al iniciar la app, pero sí cuando se está
    ejecutando para ver qué es lo que está pasando. Supongo que esto lo
    quitaríamos en producción, pero de momento es lo esperado.

--- api/src/index.js
+++ api/src/index.js
@@ -17,10 +17,20 @@ initMiddlewaresBeforeRoutes(app);
 initRoutes(app);
 initMiddlewaresAfterRoutes(app);
 
-const force = false;
+const SEQUELIZE_INITIAL_OPTIONS = {
+  force: false,
+  // https://sequelize.org/docs/v6/getting-started/#logging
+  // https://stackoverflow.com/questions/28927836/prevent-sequelize-from-outputting-sql-to-the-console-on-execution-of-query
+  logging: false,
+};
 
-const server = db.sequelize.sync({ force }).then(() => {
+const server = db.sequelize.sync(SEQUELIZE_INITIAL_OPTIONS).then(() => {
   return app.listen(PORT, () => {
+    db.sequelize.options = {
+      ...db.sequelize.options,
+      logging: true,
+    };
+
     console.log(`Server running on port ${PORT}`);
   });
 });
