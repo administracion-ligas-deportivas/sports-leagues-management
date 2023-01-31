f2288af4ef5926c4450d3e1bb7558fb06d5853af
yeicobF <jacobrdz34@gmail.com>
2023-01-31 02:07:20 -0600

    Feat(api): Add filters to /api
    Ahora podemos recibir mediante el query
    param `endpoints` el nombre de los
    endpoints que queremos obtener.
    
    Así, podemos obtener los que queramos ver específicamente.

--- api/src/routes/endpoints.js
+++ api/src/routes/endpoints.js
@@ -7,15 +7,50 @@ const endpointsRouter = require("express").Router();
 
 const { usuarios, eventos, canchas, deportes, deportivos, equipos } = ENDPOINTS;
 
+const ALL_ENDPOINTS = [
+  usuarios,
+  eventos,
+  canchas,
+  deportes,
+  deportivos,
+  equipos,
+];
+
+const DEFAULT_RESPONSE = {
+  message: "Bienvenido a la API de Administración de Ligas Deportivas",
+  lastUpdate: "Lunes, 23 de enero de 2023",
+  parentEndpoints: {
+    total: countNumberOfParentEndpoints(),
+    endpoints: Object.keys(ENDPOINTS),
+  },
+  endpoints: ALL_ENDPOINTS,
+};
+
 endpointsRouter.route("/").get((req, res) => {
-  res.json({
-    message: "Bienvenido a la API de Administración de Ligas Deportivas",
-    lastUpdate: "Lunes, 23 de enero de 2023",
-    parentEndpoints: {
-      total: countNumberOfParentEndpoints(),
-      endpoints: Object.keys(ENDPOINTS),
+  const { endpoints } = req.query;
+
+  const endpointsArray = endpoints
+    ?.split(",")
+    ?.map((endpoint) => endpoint.trim());
+
+  if (!endpoints || endpointsArray?.length === 0) {
+    res.json(DEFAULT_RESPONSE);
+    return;
+  }
+
+  const foundEndpoints = Object.values(ENDPOINTS).filter(({ endpoint }) => {
+    return endpointsArray.includes(endpoint.split("/").at(1));
+  });
+
+  console.log({ foundEndpoints, endpoints, endpointsArray });
+
+  return res.json({
+    ...DEFAULT_RESPONSE,
+    foundEndpoints: {
+      total: foundEndpoints.length,
+      endpoints: endpointsArray,
     },
-    endpoints: [usuarios, eventos, canchas, deportes, deportivos, equipos],
+    endpoints: foundEndpoints,
   });
 });
 
