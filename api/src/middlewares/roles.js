687f6660c3c02c752146076ada2c0592716edd29
yeicobF <jacobrdz34@gmail.com>
2023-01-31 03:00:59 -0600

    Feat(api): Actualizar middleware hasRoles

--- api/src/middlewares/roles.js
+++ api/src/middlewares/roles.js
@@ -2,16 +2,24 @@ const { usuariosService } = require("#src/services/index.js");
 
 const hasRoles = (...roles) => {
   return (req, res, next) => {
-    if (!req?.user) {
-      return res.status(401).json({ message: "El usuario no ha iniciado sesión" });
+    const { user } = req || {};
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ error: "El usuario no ha iniciado sesión" });
     }
 
-    if (!usuariosService.userHasRoles(req.user, roles)) {
+    if (!usuariosService.userHasRoles(user, roles)) {
+      const { id, rol } = user || {};
+
       return res.status(403).json({
         message: "El usuario requiere alguno de estos roles",
-        roles: roles.join(","),
-        usuarioId: req.user.id,
-        rolUsuario: req.user.rol,
+        expectedRoles: roles,
+        usuario: {
+          id,
+          rol,
+        },
       });
     }
 
