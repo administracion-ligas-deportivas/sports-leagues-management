74b7b8c60d50846f3bbb57f70a14b65db93062d7
yeicobF <jacobrdz34@gmail.com>
2023-01-31 02:42:09 -0600

    Feat(api): Actualizar mensajes de error para JWT

--- api/src/constants/errors/jwt.js
+++ api/src/constants/errors/jwt.js
@@ -4,12 +4,13 @@ const JWT_ERROR_HANDLERS = {
   ValidationError: (res, { message }) =>
     res.status(409).send({ error: message }),
 
-  JsonWebTokenError: (res) => res.status(401).json({ error: "Invalid Token" }),
+  JsonWebTokenError: (res) =>
+    res.status(401).json({ error: "El token no se encuentra o no es válido" }),
 
   // Podemos saber que es un token válido, pero expirado. De esta manera incluso
   // podríamos permitir ciertas acciones a usuarios que hayan estado logueados,
   // pero su sesión haya expirado.
-  TokenExpiredError: (res) => res.status(401).json({ error: "Token expired" }),
+  TokenExpiredError: (res) => res.status(401).json({ error: "El token ha expirado" }),
 };
 
 module.exports = { JWT_ERROR_HANDLERS };
