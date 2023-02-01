ccb4ab4e30f64823d9d0408f80930085a4d7b340
yeicobF <jacobrdz34@gmail.com>
2023-01-31 20:17:42 -0600

    Feat(api): Add /tipoDeEvento info

---
+++ api/src/constants/endpoints/tiposDeEvento.js
@@ -0,0 +1,13 @@
+const endpoint = "/tiposDeEvento";
+
+const tiposDeEvento = {
+  endpoint,
+  description: "Tipos de evento deportivo. Por ejemplo, torneo o liga.",
+  methods: {
+    POST: "Crear un tipo de evento deportivo",
+  },
+};
+
+module.exports = {
+  tiposDeEvento,
+};
