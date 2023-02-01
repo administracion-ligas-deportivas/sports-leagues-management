6e1d86bc5e65449b47d23c015c4b95c61ac9431a
yeicobF <jacobrdz34@gmail.com>
2023-01-31 20:39:22 -0600

    Feat(api): Add información sobre endpoints restantes
    Estos endpoints no tienen métodos, ya que no hay ningún
    endpoint implementado.

--- api/src/constants/endpoints/index.js
+++ api/src/constants/endpoints/index.js
@@ -3,16 +3,19 @@ const { deportes } = require("./deportes");
 const { deportivos } = require("./deportivos");
 const { equipos } = require("./equipos");
 const { estadisticas } = require("./estadisticas");
+const { estados } = require("./estados");
 const { eventos } = require("./eventos");
 const { formatos } = require("./formatos");
 const { login } = require("./login");
 const { me } = require("./me");
+const { migraciones } = require("./migraciones");
+const { municipios } = require("./municipios");
+const { pagos } = require("./pagos");
 const { partidos } = require("./partidos");
 const { permisos } = require("./permisos");
 const { roles } = require("./roles");
 const { tiposDeEvento } = require("./tiposDeEvento");
 const { usuarios } = require("./usuarios");
-const { estados } = require("./estados");
 
 const ENDPOINTS = {
   canchas,
@@ -25,6 +28,9 @@ const ENDPOINTS = {
   formatos,
   login,
   me,
+  migraciones,
+  municipios,
+  pagos,
   partidos,
   permisos,
   roles,
---
+++ api/src/constants/endpoints/pagos.js
@@ -0,0 +1,13 @@
+const endpoint = "/pagos";
+
+const pagos = {
+  endpoint,
+  name: "Pagos",
+  description:
+    "Pagos realizados en eventos deportivos. Estos dependen de un evento deportivo para existir y son realizados por el encargado de equipo.",
+  notes: "En desarrollo",
+};
+
+module.exports = {
+  pagos,
+};
