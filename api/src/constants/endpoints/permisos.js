03d218fe9c92b3674b04221ab39d62fa631ce9ab
yeicobF <jacobrdz34@gmail.com>
2023-01-31 20:16:59 -0600

    Feat(api): Add /permisos info

---
+++ api/src/constants/endpoints/permisos.js
@@ -0,0 +1,12 @@
+const endpoint = "/permisos";
+
+const permisos = {
+  endpoint,
+  description: "Permisos del sistema. Estos son asignados a los roles.",
+  notes: "En desarrollo",
+  methods: null,
+};
+
+module.exports = {
+  permisos,
+};
