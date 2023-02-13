0ab85e69579ada155e2baf73020773f087b95adf
gus <agustin.sanchez3150@gmail.com>
2023-02-12 20:57:16 -0600

    error en getPartidoById

--- api/src/routes/partidos/index.js
+++ api/src/routes/partidos/index.js
@@ -1,5 +1,23 @@
-const { partidosRouter } = require("./partidos");
+const {
+  getPartidos,
+  getPartidoById,
+  updatePartido,
+  deletePartido,
+} = require("#src/controllers/partidos/index.js");
 
-module.exports = {
-  partidosRouter,
-};
+const { initRouterSubroutes } = require("#src/utils/routes.js");
+
+const routes = [getPartidos, getPartidoById, updatePartido, deletePartido];
+
+const partidosRouter = require("express").Router();
+
+partidosRouter.route("/").get(getPartidos);
+partidosRouter
+  .route("/:partidoId")
+  .get(getPartidoById)
+  .put(updatePartido)
+  .delete(deletePartido);
+
+initRouterSubroutes(partidosRouter, routes);
+
+module.exports = { partidosRouter };
