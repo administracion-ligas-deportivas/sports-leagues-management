c7ca41b01eeaa1f2daa1ec94e2dfd1cbf7ce2a41
gus <agustin.sanchez3150@gmail.com>
2023-02-15 05:36:11 -0600

    añadir middleware existe partido a index

--- api/src/services/partidos/index.js
+++ api/src/services/partidos/index.js
@@ -1,8 +1,10 @@
 // const { partidosRepository } = require('../../repositories/partidos');
 const partidos = require("./partidos");
+const equipos = require("./equipos");
 
 const partidosService = {
   ...partidos,
+  ...equipos,
 };
 
 module.exports = { partidosService };
