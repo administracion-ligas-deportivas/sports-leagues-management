f8aed27014e3c1bdf2cba9079492376381503b7d
yeicobF <jacobrdz34@gmail.com>
2023-02-23 13:48:31 -0600

    Build(dash): Cambiar nombre de env var

--- api/src/config/dev.js
+++ api/src/config/dev.js
@@ -1,14 +1,14 @@
 const process = require("process");
 
 const DEV_DB_USERNAME = process.env.DEV_DB_USERNAME || "root";
-const DEV_DB_HOST = process.env.DEV_DB_HOST || "localhost";
+const DEV_DB_HOSTNAME = process.env.DEV_DB_HOSTNAME || "localhost";
 const DEV_DB_DATABASE = process.env.DEV_DB_DATABASE || "ligas_deportivas";
 const DEV_DB_PORT = process.env.DEV_DB_PORT || 3306;
 const DEV_DB_PASSWORD = process.env.DEV_DB_PASSWORD;
 
 module.exports = {
   DEV_DB_USERNAME,
-  DEV_DB_HOST,
+  DEV_DB_HOSTNAME,
   DEV_DB_DATABASE,
   DEV_DB_PORT,
   DEV_DB_PASSWORD,
