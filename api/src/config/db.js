48f7a12830ed1cd5c539b4cc867467ede984f212
yeicobF <jacobrdz34@gmail.com>
2023-02-23 13:48:59 -0600

    Refactor(dash): Modificar config de BD

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -1,7 +1,7 @@
 const {
   DEV_DB_PASSWORD,
   DEV_DB_DATABASE,
-  DEV_DB_HOST,
+  DEV_DB_HOSTNAME,
   DEV_DB_USERNAME,
 } = require("./dev");
 
@@ -14,29 +14,32 @@ const define = {
   allowNull: false,
 };
 
+const DEFAULT_CONFIG = {
+  define,
+  dialect,
+};
+
 module.exports = {
   development: {
-    host: DEV_DB_HOST,
+    ...DEFAULT_CONFIG,
+    host: DEV_DB_HOSTNAME,
     username: DEV_DB_USERNAME,
     password: DEV_DB_PASSWORD,
     database: DEV_DB_DATABASE,
-    dialect,
-    define,
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect,
-    define,
+    ...DEFAULT_CONFIG,
+    username: process.env.TEST_DB_USERNAME,
+    password: process.env.TEST_DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.TEST_DB_HOSTNAME,
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect,
-    define,
+    ...DEFAULT_CONFIG,
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
   },
 };
