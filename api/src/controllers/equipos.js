402fe72c53b6beb0e44c13e47e13467462440f75
yeicobF <jacobrdz34@gmail.com>
2023-02-24 09:47:29 -0600

    Feat(api): Inscribir jugador a equipo sin validaciones

--- api/src/controllers/equipos.js
+++ api/src/controllers/equipos.js
@@ -17,7 +17,40 @@ const getEquipoById = async (req, res) => {
   return res.json(foundEquipo);
 };
 
+/**
+ * TODO - Revisar que el jugador tenga el rol de USUARIO.
+ */
+const inscribirJugadorEnEquipo = async (req, res) => {
+  const { uuid: equipoUuid } = req.params;
+  const { id: jugadorId } = req.user;
+
+  try {
+    const jugadoresInscritos = await equiposService.inscribirJugadorEnEquipo(
+      equipoUuid,
+      jugadorId
+    );
+
+    console.log({ jugadoresInscritos });
+
+    if (!jugadoresInscritos) {
+      return res.status(400).json({
+        error: "No se pudo inscribir al jugador.",
+      });
+    }
+
+    return res.json({
+      message: "Jugador inscrito exitosamente.",
+    });
+  } catch {
+    return res.status(400).json({
+      error: "No se pudo inscribir al jugador.",
+      // error: "El jugador ya está inscrito en el equipo.",
+    });
+  }
+};
+
 module.exports = {
   getEquipos,
   getEquipoById,
+  inscribirJugadorEnEquipo,
 };
--- api/src/routes/equipos.js
+++ api/src/routes/equipos.js
@@ -1,8 +1,12 @@
 const equiposRouter = require("express").Router();
 
-const { getEquipos, getEquipoById } = require("#src/controllers/equipos.js");
+const { getEquipos, getEquipoById, inscribirJugadorEnEquipo } = require("#src/controllers/equipos.js");
+const { userAuthenticator } = require("#src/middlewares/index.js");
 
 equiposRouter.route("/").get(getEquipos);
 equiposRouter.route("/:equipoId").get(getEquipoById);
+// /equipos/:equipoUuid/jugadores
+// Inscribir al usuario actual como jugador de un equipo.
+equiposRouter.route("/:uuid/jugadores").post(userAuthenticator, inscribirJugadorEnEquipo);
 
 module.exports = { equiposRouter };
