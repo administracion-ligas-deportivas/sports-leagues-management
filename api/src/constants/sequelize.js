629ca4a249caddbd11f9786489ad644c9f7b0889
yeicobF <jacobrdz34@gmail.com>
2023-02-23 19:13:53 -0600

    Fix(api): Export sequelize constants with commonjs

--- api/src/constants/sequelize.js
+++ api/src/constants/sequelize.js
@@ -3,4 +3,4 @@ const DB_CONFIG_BASE_PATH = `${PROJECT_ROOT_PATH}/db`;
 
 const PATH_KEYS = ["models-path", "seeders-path", "migrations-path"];
 
-export { PROJECT_ROOT_PATH, DB_CONFIG_BASE_PATH, PATH_KEYS };
+module.exports = { PROJECT_ROOT_PATH, DB_CONFIG_BASE_PATH, PATH_KEYS };
