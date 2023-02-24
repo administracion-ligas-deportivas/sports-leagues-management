76f9feea0f10470f29b8071374f4465898b02b93
yeicobF <jacobrdz34@gmail.com>
2023-02-23 20:59:49 -0600

    Build(api): Change default dev db name

--- api/src/config/dev.js
+++ api/src/config/dev.js
@@ -2,7 +2,7 @@ const process = require("process");
 
 const DEV_DB_USERNAME = process.env.DEV_DB_USERNAME || "root";
 const DEV_DB_HOSTNAME = process.env.DEV_DB_HOSTNAME || "localhost";
-const DEV_DB_DATABASE = process.env.DEV_DB_DATABASE || "ligas_deportivas";
+const DEV_DB_DATABASE = process.env.DEV_DB_DATABASE || "ligas_deportivas_dev";
 const DEV_DB_PORT = process.env.DEV_DB_PORT || 3306;
 const DEV_DB_PASSWORD = process.env.DEV_DB_PASSWORD;
 
