e3654a3ce6366817e96f6c3e3d9c441e11b051ab
yeicobF <jacobrdz34@gmail.com>
2023-02-27 06:27:34 -0600

    Refactor(app): Change imports in validationts

--- app/src/validations/deportivoSchema.js
+++ app/src/validations/deportivoSchema.js
@@ -1,12 +1,13 @@
-import { date, mixed, object, string } from "yup";
+import { number, object, string } from "yup";
+import { transformNaN } from "@/utils";
 
 // https://stackoverflow.com/a/60726477/13562806
 export const deportivoSchema = object({
-    nombre: string().required("Nombre requerido"),
-    calle: string().required("Calle requerida"),
-    colonia: string().required("Colonia requerida"),
-    codigoPostal: string().required("Código postal requerido"),
-    numeroExterior: string().required("Número exterior requerido"),
-    numeroInterior: string(),
-    municipioId: number().transform(transformNaN).required("Municipio requerido"),
+  nombre: string().required("Nombre requerido"),
+  calle: string().required("Calle requerida"),
+  colonia: string().required("Colonia requerida"),
+  codigoPostal: string().required("Código postal requerido"),
+  numeroExterior: string().required("Número exterior requerido"),
+  numeroInterior: string(),
+  municipioId: number().transform(transformNaN).required("Municipio requerido"),
 });
--- app/src/validations/domicilioUsuarioSchema.js
+++ app/src/validations/domicilioUsuarioSchema.js
@@ -1,7 +1,5 @@
 import { number, object, string } from "yup";
-
-// https://github.com/jquense/yup/issues/971#issuecomment-675528093
-const transformNaN = (value) => (isNaN(value) ? undefined : value);
+import { transformNaN } from "@/utils";
 
 export const domicilioUsuarioSchema = object({
   calle: string().required("Calle requerida"),
