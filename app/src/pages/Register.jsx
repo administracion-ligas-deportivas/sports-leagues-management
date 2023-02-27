593f3631c4010f9a03e2ce2f6e10fe98859da47e
yeicobF <jacobrdz34@gmail.com>
2023-02-27 07:35:54 -0600

    Feat(app): Improve register page creation

--- app/src/pages/Register.jsx
+++ app/src/pages/Register.jsx
@@ -1,38 +1,38 @@
-import { useForm } from "react-hook-form";
-import { Link, useNavigate } from "react-router-dom";
-import { createUser } from "@/services/usuarios";
-
 import {
-  TextField,
-  Button,
-  Alert,
   Accordion,
-  AccordionSummary,
   AccordionDetails,
-  Typography,
-  Stack,
+  AccordionSummary,
+  Alert,
   Autocomplete,
-  Select,
-  InputLabel,
-  MenuItem,
+  Button,
   FormControl,
   FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  TextField,
+  Typography,
 } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 import { DatePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import {
+  GENEROS,
+  REGISTER_ADDRESS,
+  REGISTER_FORM_FIELDS,
+} from "@/constants";
+import { Link, useNavigate } from "react-router-dom";
+import { capitalizeFirstLetter, getOnlyDate } from "@/utils";
+import { useEffect, useState } from "react";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateTime } from "luxon";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { LoginSignupStyles } from "@/styles";
+import { registerSchema } from "@/validations";
+import { useEstados } from "@/hooks";
+import { useForm } from "react-hook-form";
+import { usuariosService } from "@/services";
 import { yupResolver } from "@hookform/resolvers/yup";
-
-import myimg from "/img-login.png";
-import styles from "@/styles/LoginSignup.module.css";
-import { registerSchema } from "@/validations/registerSchema";
-import { useState } from "react";
-import { useEstados } from "@/hooks/useEstados";
-import { useEffect } from "react";
-import { GENEROS } from "@/constants/usuario";
-import { capitalizeFirstLetter } from "@/utils/capitalize";
-import { getOnlyDate } from "@/utils/date";
 
 function Signup() {
   const {
@@ -54,50 +54,6 @@ function Signup() {
     useEstados();
   const [selectedMunicipio, setSelectedMunicipio] = useState(null);
   const [serverError, setServerError] = useState("");
-
-  const textFields = [
-    {
-      id: "user-input-pass",
-      name: "nombre",
-      label: "Nombre(s)",
-    },
-    { id: "user-input-pass", name: "apellido", label: "Apellido(s)" },
-    {
-      id: "user-input",
-      type: "tel",
-      name: "telefono",
-      label: "Teléfono",
-    },
-  ];
-
-  const passwordFields = [
-    {
-      id: "user-input-pass",
-      name: "correo",
-      label: "Correo",
-    },
-    {
-      id: "user-input-pass",
-      type: "password",
-      name: "password",
-      label: "Contraseña",
-    },
-    {
-      id: "user-input-pass",
-      type: "password",
-      name: "confirmPassword",
-      label: "Confirmar contraseña",
-    },
-  ];
-
-  const domicilioFields = {
-    calle: {
-      id: "user-input-calle",
-      type: "text",
-      name: "calle",
-      label: "Calle",
-    },
-  };
 
   useEffect(() => {
     if (!watchEstadoId) {
@@ -161,7 +117,8 @@ function Signup() {
 
     console.log({ user });
 
-    createUser(user)
+    usuariosService
+      .createUser(user)
       .then(() => {
         navigate("/");
       })
@@ -196,206 +153,198 @@ function Signup() {
     return { ...registerProp(prop), ...fieldErrors };
   };
 
-  const imageClasses = [styles.container, "hidden", "sm:flex"].join(" ");
+  const imageClasses = [LoginSignupStyles.container, "hidden", "sm:flex"].join(" ");
 
   // stepper
   // stepper
   return (
-    <div className="flex flex-col justify-between h-full">
-      <section className={[styles.mainContainerRegister]}>
-        <section className={imageClasses}>
-          <h1 className={styles.titleLeague}>
-            Administración de Ligas Deportivas
-          </h1>
-          <img src={myimg} alt="Register_image" width="300px" height="300px" />
-        </section>
-        <form
-          onSubmit={handleSubmit(registerUser)}
-          className={[styles.container, styles.signupContainer].join(" ")}
-        >
-          <Link to="/login">Iniciar sesión</Link>
-          <h1 className={styles.titlePageSignUp}>Regístrate</h1>
+    <section className={[LoginSignupStyles.mainContainerRegister]}>
+      <section className={imageClasses}>
+        <h1 className={LoginSignupStyles.titleLeague}>
+          Administración de Ligas Deportivas
+        </h1>
+        <img src="/img-login.png" alt="Baseball player swinging a bat" width="300" height="300" />
+      </section>
+      <form
+        onSubmit={handleSubmit(registerUser)}
+        className={[LoginSignupStyles.container, LoginSignupStyles.signupContainer].join(" ")}
+      >
+        <Link to="/login">Iniciar sesión</Link>
+        <h1 className={LoginSignupStyles.titlePageSignUp}>Regístrate</h1>
 
-          {passwordFields.map((textField) => {
-            return (
-              <TextField
-                {...textField}
-                key={textField?.name}
-                {...register(textField.name)}
+        {REGISTER_FORM_FIELDS.password.map((field) => {
+          return (
+            <TextField
+              {...field}
+              key={field.id}
+              autoComplete="new-password"
+              {...register(field.name)}
+              fullWidth
+            />
+          );
+        })}
+
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="datos-personales"
+            id="datos-personales"
+          >
+            <Typography sx={{ fontWeight: "500" }}>
+              Datos personales
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            {/* Primer step */}
+            {REGISTER_FORM_FIELDS.text.map((field) => {
+              return (
+                <TextField
+                  {...field}
+                  key={field?.name}
+                  {...register(field.name)}
+                  fullWidth
+                  sx={{ marginTop: "1.5em" }}
+                />
+              );
+            })}
+            <div className={LoginSignupStyles.genderDataContainer}>
+              <FormControl>
+                <InputLabel id="genero">Género</InputLabel>
+                <Select
+                  labelId="genero"
+                  name="genero"
+                  label="Género"
+                  {...register("genero", { setHelperText: false })}
+                >
+                  {GENEROS.map((genero) => {
+                    const capitalized = capitalizeFirstLetter(genero);
+                    return (
+                      <MenuItem value={genero} key={genero}>
+                        {capitalized}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+                {errors?.genero && (
+                  <FormHelperText
+                    {...setFieldErrors("genero", { setHelperText: false })}
+                  >
+                    {errors?.genero?.message}
+                  </FormHelperText>
+                )}
+              </FormControl>
+
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Fecha de nacimiento"
+                  inputFormat="DD/MM/YYYY"
+                  defaultValue={DateTime.now()}
+                  {...register("fechaNacimiento")}
+                  value={watch("fechaNacimiento")}
+                  onChange={(newValue) => {
+                    setValue("fechaNacimiento", newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </div>
+          </AccordionDetails>
+        </Accordion>
+
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="direccion"
+            id="direccion"
+          >
+            <Typography sx={{ fontWeight: "500" }}>
+              Datos de dirección
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Stack direction="row" spacing={2}>
+              <Autocomplete
+                {...register(
+                  REGISTER_FORM_FIELDS.address.estado.id, 
+                  { setErrors: false }
+                )}
                 fullWidth
-              />
-            );
-          })}
-
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="datos-personales"
-              id="datos-personales"
-            >
-              <Typography
-                sx = {{fontWeight: "500"}}> Datos personales </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              {/* Primer step */}
-              {textFields.map((textFields) => {
-                return (
+                options={estados}
+                getOptionLabel={(option) => option.nombre}
+                onChange={(event, newValue) => {
+                  console.log({ newValue });
+                  setValue(REGISTER_FORM_FIELDS.address.estado.id, newValue?.id);
+                }}
+                renderInput={(params) => (
                   <TextField
-                    {...textFields}
-                    key={textFields?.name}
-                    {...register(textFields.name)}
-                    fullWidth
-                    sx={{ marginTop: "1.5em" }}
+                    {...params}
+                    label={REGISTER_FORM_FIELDS.address.estado.label}
+                    {...setFieldErrors(
+                      REGISTER_FORM_FIELDS.address.estado.id
+                    )}
                   />
+                )}
+              />
+
+              <Autocomplete
+                {...register(
+                  REGISTER_FORM_FIELDS.address.municipio.id, 
+                  { setErrors: false }
+                )}
+                onChange={(event, newValue) => {
+                  console.log({ municipio: newValue });
+                  setValue(REGISTER_FORM_FIELDS.address.municipio.id, newValue?.id);
+                }}
+                fullWidth
+                value={selectedMunicipio}
+                options={currentEstado?.municipios || []}
+                getOptionLabel={(option) => option?.nombre}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label={REGISTER_FORM_FIELDS.address.municipio.label}
+                    {...setFieldErrors(
+                      REGISTER_FORM_FIELDS.address.municipio.id
+                    )}
+                  />
+                )}
+              />
+            </Stack>
+            <TextField
+              {...REGISTER_FORM_FIELDS.address.calle}
+              {...register(REGISTER_FORM_FIELDS.address.calle.id)}
+              fullWidth
+              sx={{ marginTop: "1.5em" }}
+            />
+            {
+              Object.values(REGISTER_ADDRESS).map((addressProperty, index) => {
+                return (
+                  <Stack direction="row" spacing={2} sx={{ marginTop: "1.5em" }} key={index}>
+                    {
+                      addressProperty.map((field) => {
+                        return (
+                          <TextField
+                            {...register(field.id)}
+                            {...field}
+                            fullWidth
+                            key={field?.id}
+                          />
+                        );
+                      })
+                    }
+                  </Stack>
                 );
               })}
-              <div className={styles.genderDataContainer}>
-                <FormControl>
-                  <InputLabel id="select-genero">Género</InputLabel>
-                  <Select
-                    labelId="select-genero"
-                    name="genero"
-                    label="Género"
-                    {...register("genero", { setHelperText: false })}
-                  >
-                    {GENEROS.map((genero) => {
-                      const capitalized = capitalizeFirstLetter(genero);
-                      return (
-                        <MenuItem value={genero} key={genero}>
-                          {capitalized}
-                        </MenuItem>
-                      );
-                    })}
-                  </Select>
-                  {errors?.genero && (
-                    <FormHelperText
-                      {...setFieldErrors("genero", { setHelperText: false })}
-                    >
-                      {errors?.genero?.message}
-                    </FormHelperText>
-                  )}
-                </FormControl>
+          </AccordionDetails>
+        </Accordion>
 
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    label="Fecha de nacimiento"
-                    inputFormat="DD/MM/YYYY"
-                    defaultValue={DateTime.now()}
-                    {...register("fechaNacimiento")}
-                    value={watch("fechaNacimiento")}
-                    onChange={(newValue) => {
-                      setValue("fechaNacimiento", newValue);
-                    }}
-                    renderInput={(params) => <TextField {...params} />}
-                  />
-                </LocalizationProvider>
-              </div>
-            </AccordionDetails>
-          </Accordion>
+        {serverError && <Alert severity="error">{serverError}</Alert>}
 
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="direccion"
-              id="direccion"
-            >
-              <Typography
-                sx = {{fontWeight: "500"}}> Datos de dirección </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Stack direction="row" spacing={2}>
-                <Autocomplete
-                  {...register("estadoId", { setErrors: false })}
-                  fullWidth
-                  id="buscar-estadoId"
-                  options={estados}
-                  getOptionLabel={(option) => option.nombre}
-                  onChange={(event, newValue) => {
-                    console.log({ newValue });
-                    setValue("estadoId", newValue?.id);
-                  }}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Selecciona un estado"
-                      {...setFieldErrors("estadoId")}
-                    />
-                  )}
-                />
-
-                <Autocomplete
-                  {...register("municipioId", { setErrors: false })}
-                  onChange={(event, newValue) => {
-                    console.log({ municipio: newValue });
-                    setValue("municipioId", newValue?.id);
-                  }}
-                  fullWidth
-                  id="buscar-municipio"
-                  value={selectedMunicipio}
-                  options={currentEstado?.municipios || []}
-                  getOptionLabel={(option) => option?.nombre}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      label="Selecciona un municipio"
-                      {...setFieldErrors("municipioId")}
-                    />
-                  )}
-                />
-              </Stack>
-              <TextField
-                {...domicilioFields.calle}
-                {...register("calle")}
-                fullWidth
-                sx={{ marginTop: "1.5em" }}
-              />
-              <Stack direction="row" spacing={2} sx={{ marginTop: "1.5em" }}>
-                <TextField
-                  {...register("numeroExterior")}
-                  fullWidth
-                  id="user-input-numero-exterior"
-                  type="text"
-                  name="numeroExterior"
-                  label="Número Exterior"
-                />
-                <TextField
-                  {...register("numeroInterior")}
-                  fullWidth
-                  id="user-input-numero-interior"
-                  type="text"
-                  name="numeroInterior"
-                  label="Número Interior"
-                />
-              </Stack>
-              <Stack direction="row" spacing={2} sx={{ marginTop: "1.5em" }}>
-                <TextField
-                  {...register("colonia")}
-                  fullWidth
-                  id="user-input-colonia"
-                  type="text"
-                  name="colonia"
-                  label="Colonia"
-                />
-                <TextField
-                  {...register("codigoPostal")}
-                  fullWidth
-                  id="user-input-codigo-postal"
-                  type="text"
-                  name="codigoPostal"
-                  label="Código Postal"
-                />
-              </Stack>
-            </AccordionDetails>
-          </Accordion>
-
-          {serverError && <Alert severity="error">{serverError}</Alert>}
-
-          <Button variant="contained" type="submit">
-            Registrar
-          </Button>
-        </form>
-      </section>
-    </div>
+        <Button variant="contained" type="submit">
+          Registrar
+        </Button>
+      </form>
+    </section>
   );
 }
 
